49dbf10cc501a8ba2c916f74e0ec5284128db377
Anton Malay <romory999@gmail.com>
2020-10-29 21:17:38 +0400

    Update scripts.js

--- yoga/js/scripts.js
+++ yoga/js/scripts.js
@@ -1,21 +1,20 @@
 window.addEventListener('DOMContentLoaded', function() { // описывает то что js срикпт будет выполняться когда все ДОМ дерево загружено
     'use strict';
-// дальше идет тело скрипта
+
 // 1. обьявляем переменные
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
-// продумываем что мы делаем
-// сначало то что нам не надо мы должны скрыть
-// потом при клике на кнопку мы долны показывать на определенный контент
 
-    function hideTabContent(a) { //функция который скрывается элементы
+
+//функция который скрывается элементы
+    function hideTabContent(a) { 
         for (let i = a; i < tabContent.length; i++) {
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
         }
     }
-    hideTabContent(1); // то есть мы скрыли все кроме первого таба
+    hideTabContent(1); 
 
 // далее функция который будет показывать
     function showTabContent(b) {
